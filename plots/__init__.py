af578ef222b1afcd22934af9fe90e0b52280f402
EthanTreg <ethan@treggal.com>
2025-02-21 09:55:57 +0100

    Added all plots to be imported from module rather than individual scripts.

--- plots/__init__.py
+++ plots/__init__.py
@@ -7,3 +7,14 @@ import logging
 __version__ = '0.0.1'
 __author__ = 'Ethan Tregidga'
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.WARNING)
+
+try:
+    from plots.clustering import PlotClusters
+    from plots.grids import PlotConfusion, PlotPearson
+    from plots.misc import PlotDistributions, PlotImages, PlotSaliency
+    from plots.pair_plots import PlotParamPairs, PlotParamPairComparison
+    from plots.single_plots import PlotComparison, PlotPerformance, PlotPlots, PlotDistribution
+except ModuleNotFoundError:
+    pass
+except ImportError:
+    pass
